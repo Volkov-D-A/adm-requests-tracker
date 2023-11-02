0146c18cd65b294ce9820b9233a983791f59ad36
Volkov-D-A <dimas.volkov@gmail.com>
2023-11-02 13:43:40 +0500

    added migrations :card_file_box:

---
+++ admreq-golang-backend/migrations/1_initialize_schema.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE requsers, reqtickets, reqcomments, reqexecutors;
---
+++ admreq-golang-backend/migrations/1_initialize_schema.up.sql
@@ -0,0 +1,38 @@
+CREATE TABLE requsers (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    CONSTRAINT id_requsers PRIMARY KEY (id),
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    user_role VARCHAR NOT NULL DEFAULT 'user',
+    user_login VARCHAR UNIQUE NOT NULL,
+    user_pass VARCHAR NOT NULL
+);
+
+CREATE TABLE reqtickets (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    CONSTRAINT id_reqtickets PRIMARY KEY (id),
+    user_id UUID NOT NULL REFERENCES requsers (id),
+    req_text TEXT NOT NULL,
+    created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'Asia/Yekaterinburg'),
+    finished_at TIMESTAMP(0) WITHOUT TIME ZONE,
+    finished_user_id UUID REFERENCES requsers (id),
+    finished_comment TEXT,
+    req_important BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE reqcomments (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    CONSTRAINT id_reqcomments PRIMARY KEY (id),
+    req_id UUID NOT NULL REFERENCES reqtickets (id),
+    user_id UUID NOT NULL REFERENCES requsers (id),
+    comm_text TEXT NOT NULL,
+    created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'Asia/Yekaterinburg')
+);
+
+CREATE TABLE reqexecutors (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    CONSTRAINT id_reqexecutors PRIMARY KEY (id),
+    req_id UUID NOT NULL REFERENCES reqtickets (id),
+    user_id UUID NOT NULL REFERENCES requsers (id),
+    ex_set_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'Asia/Yekaterinburg')
+);
