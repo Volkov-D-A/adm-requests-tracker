7645e33a2875e62a7478b425126467d0a5d52daa
Dimas Volkov <dimas.volkov@gmail.com>
2024-07-11 10:41:32 +0500

    added dockerfile to build backend

--- admreq-golang-backend/pkg/config/config.go
+++ admreq-golang-backend/pkg/config/config.go
@@ -12,10 +12,7 @@ type Config struct {
 }
 
 func GetConfig() (*Config, error) {
-	config_path := os.Getenv("CONFIG_PATH")
-	if config_path == "" {
-		return nil, fmt.Errorf("CONFIG_PATH is not set")
-	}
+	config_path := "config/config.yaml"
 
 	if _, err := os.Stat(config_path); os.IsNotExist(err) {
 		return nil, fmt.Errorf("config file does not exist: %s", config_path)
@@ -24,7 +21,7 @@ func GetConfig() (*Config, error) {
 	var conf config
 
 	if err := cleanenv.ReadConfig(config_path, &conf); err != nil {
-		return nil, fmt.Errorf("Error while reading config file: %v", err)
+		return nil, fmt.Errorf("error while reading config file: %v", err)
 	}
 
 	return &Config{&conf}, nil
