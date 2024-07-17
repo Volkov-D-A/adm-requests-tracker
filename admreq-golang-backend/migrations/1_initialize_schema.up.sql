7e469bdf6b09995b576e39e77965f38fae580f18
Dimas Volkov <dimas.volkov@gmail.com>
2024-07-17 14:12:44 +0500

    fixed database structure

--- admreq-golang-backend/migrations/1_initialize_schema.up.sql
+++ admreq-golang-backend/migrations/1_initialize_schema.up.sql
@@ -1,10 +1,18 @@
+CREATE TABLE departments (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    CONSTRAINT id_departments PRIMARY KEY (id),
+    department_name VARCHAR UNIQUE NOT NULL,
+    department_active BOOLEAN NOT NULL DEFAULT TRUE,
+    department_work BOOLEAN NOT NULL DEFAULT FALSE
+);
+
 CREATE TABLE requsers (
     id UUID NOT NULL DEFAULT gen_random_uuid(),
     CONSTRAINT id_requsers PRIMARY KEY (id),
     firstname VARCHAR NOT NULL,
     lastname VARCHAR NOT NULL,
     surname VARCHAR NOT NULL,
-    department VARCHAR NOT NULL,
+    department UUID NOT NULL REFERENCES departments (id),
     user_role VARCHAR NOT NULL DEFAULT 'user',
     user_login VARCHAR UNIQUE NOT NULL,
     user_pass VARCHAR NOT NULL,
@@ -16,6 +24,7 @@ CREATE TABLE reqtickets (
     CONSTRAINT id_reqtickets PRIMARY KEY (id),
     user_id UUID NOT NULL REFERENCES requsers (id),
     req_text TEXT NOT NULL,
+    target_department UUID NOT NULL REFERENCES departments (id),
     created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'Asia/Yekaterinburg'),
     finished_at TIMESTAMP(0) WITHOUT TIME ZONE,
     finish_before TIMESTAMP(0) WITHOUT TIME ZONE,
@@ -32,12 +41,4 @@ CREATE TABLE reqcomments (
     user_id UUID NOT NULL REFERENCES requsers (id),
     comm_text TEXT NOT NULL,
     created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'Asia/Yekaterinburg')
-);
-
-CREATE TABLE viewscomments (
-    id UUID NOT NULL DEFAULT gen_random_uuid(),
-    CONSTRAINT id_viewscomments PRIMARY KEY (id),
-    comm_id UUID NOT NULL REFERENCES reqcomments (id),
-    user_id UUID NOT NULL REFERENCES requsers (id),
-    view_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'Asia/Yekaterinburg')
 );
