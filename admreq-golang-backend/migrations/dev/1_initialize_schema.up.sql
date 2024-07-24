019fcebf7d436612344290cc3c2ee2b06d7a4c10
Dimas Volkov <dimas.volkov@gmail.com>
2024-07-24 10:59:14 +0500

    added log actions table to dev migrations

--- admreq-golang-backend/migrations/dev/1_initialize_schema.up.sql
+++ admreq-golang-backend/migrations/dev/1_initialize_schema.up.sql
@@ -42,3 +42,14 @@ CREATE TABLE reqcomments (
     comm_text TEXT NOT NULL,
     created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'Asia/Yekaterinburg')
 );
+
+CREATE TABLE actions (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    CONSTRAINT id_reqcomments PRIMARY KEY (id),
+    action_subject UUID NOT NULL REFERENCES requsers (id),
+    action_object TEXT NOT NULL,
+    action_string TEXT NOT NULL,
+    action_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'Asia/Yekaterinburg'),
+    action_result BOOLEAN NOT NULL DEFAULT TRUE,
+    action_info TEXT
+);
