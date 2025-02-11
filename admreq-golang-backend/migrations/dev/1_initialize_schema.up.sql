8124351656fbea3f3d85e72c5c9e4bf9ec8840ea
Dimas Volkov <dimas.volkov@gmail.com>
2025-02-11 15:46:41 +0500

    updated dev migration

--- admreq-golang-backend/migrations/dev/1_initialize_schema.up.sql
+++ admreq-golang-backend/migrations/dev/1_initialize_schema.up.sql
@@ -60,7 +60,8 @@ CREATE TABLE readticket (
     CONSTRAINT id_readticket PRIMARY KEY (id),
     req_id UUID NOT NULL REFERENCES reqtickets (id),
     user_id UUID NOT NULL REFERENCES requsers (id),
-    lastread TIMESTAMP(0) WITHOUT TIME ZONE
+    lastread TIMESTAMP(0) WITHOUT TIME ZONE,
+    UNIQUE (req_id, user_id)
 );
 
 CREATE TABLE actions (
